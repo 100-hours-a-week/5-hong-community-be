2769ec493ba9e86c3bfd72f494ca5c93367b5a5b
JiHongKim98 <kinjihong9598@gmail.com>
2024-05-30 02:01:23 +0900

    feat: Add session storage to mysql

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,17 @@
 const mysql = require('mysql2');
 
-const pool = mysql.createPool({
+const mysqlConfig = {
   host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
+  user: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
   connectionLimit: 10,  // 일단 10개의 연결을 설정해둠
   queueLimit: 0,  // 대기열 제한 x
-});
+};
 
-module.exports = pool.promise();
+const pool = mysql.createPool(mysqlConfig).promise();
+
+module.exports = {
+  mysqlConfig,
+  pool,
+};
--- src/config/sessionConfig.js
+++ src/config/sessionConfig.js
@@ -1,4 +1,15 @@
+// https://www.npmjs.com/package/express-mysql-session
+
+const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
+const { mysqlConfig } = require('./database');
+
+// session configuration
 const defaultKey = 'defaultKey)_(defaultKeys';
+
+const store = new MySQLStore({
+  ...mysqlConfig,
+});
 
 const sessionOptions = {
   secret: process.env.SECRET_KEY || defaultKey,
@@ -7,6 +18,7 @@ const sessionOptions = {
   cookie: {
     maxAge: 24 * 60 * 60 * 1000,  // 1일
   },
+  store,
 };
 
 module.exports = sessionOptions;

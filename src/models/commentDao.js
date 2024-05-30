b324dc91cba53c9cb8302a22bba1059c79e1cf98
JiHongKim98 <kinjihong9598@gmail.com>
2024-05-31 01:41:25 +0900

    refact: Update comment dao class to use mysql database

--- src/models/commentDao.js
+++ src/models/commentDao.js
@@ -1,88 +1,109 @@
-const { comments } = require('./data');
+const { query } = require('../config/database');
+const { conversionUtils } = require('../utils');
 
-// TODO: pageSize 설정 .env or property 로 이동 (임시 : 10)
-const pageSize = 10;
-let sequenceId = 37;  // 시작 ID
+// command executor
 
-// { commentsId, contents, createdAt, ownerId, postsId, isVisible }
-const save = (comment) => {
-  // commentsId 가 없는 경우 (insert)
-  if (!comment.commentsId) {
-    comment.commentsId = sequenceId;
-    comments.push(comment);
-    sequenceId++;
-    return;
+const save = async ({ conn, comment }) => {
+  if (!comment.commentId) {
+    const [result] = await conn.query(  // insert query
+      `INSERT INTO comment (owner_id, post_id, contents)
+       VALUES (?, ?, ?)`,
+      [comment.ownerId, comment.postId, comment.contents],
+    );
+    return result.insertId;
   }
 
-  // commentsId 가 있는 경우 (update)
-  const commentsId = comment.commentsId;
-  const index = comments.findIndex(_comment =>
-    _comment.commentsId === commentsId &&
-    _comment.isVisible,
+  const [result] = await conn.query( // update query
+    `UPDATE comment
+     SET contents = ?
+     WHERE comment_id = ?`,
+    [comment.contents, comment.commentId],
   );
-
-  if (index !== -1) {
-    comments[index] = comment;
-    return;
-  }
-  throw new Error('존재하지 않는 댓글 - 서버 (로직) 오류');
+  return result.insertId;
 };
 
-// 실제로 삭제하지 않고 isVisible 만 false
-const deleteById = (commentsId) => {
-  const index = comments.findIndex((comment) =>
-    comment.commentsId === commentsId &&
-    comment.isVisible,
+const deleteById = async ({ conn, id }) => {
+  const [result] = await conn.query(
+    `UPDATE comment
+     SET is_visible = FALSE
+     WHERE comment_id = ?`,
+    [id],
   );
-
-  if (index !== -1) {  // 해당 게시글을 안보이게 변경
-    comments[index].isVisible = false;
-    return;
-  }
-  throw new Error('존재하지 않는 댓글 - 서버 (로직) 오류');
+  return result.insertId;
 };
 
-const findById = (commentsId) => {
-  return comments.find((comment) =>
-    comment.commentsId === commentsId &&
-    comment.isVisible,
-  );
+// query executor
+
+const findAllByPostIdOrderByIdDesc = async ({ conn, postId, cursor, limit = 11 }) => {
+  const orderAndLimitClause = `
+    ORDER BY c.comment_id DESC
+    LIMIT ?
+  `;
+
+  const sql = `SELECT c.*, m.member_id, m.nickname, m.profile_image
+               FROM comment c
+                        LEFT JOIN member m ON c.owner_id = m.member_id
+               WHERE c.post_id = ?
+                 AND c.is_visible = TRUE
+                   ${cursor ? `AND c.comment_id <= ?` : ''} ${orderAndLimitClause}`;
+  const values = cursor ? [postId, cursor, limit] : [postId, limit];
+
+  let rows;
+  if (conn)
+    rows = await conn.query(sql, values);
+  else
+    rows = await query(sql, values);
+
+  return rows.map((row) => {
+    const { memberId, nickname, profileImage, ...rest } = conversionUtils.snakeToCamel(row);
+    return {
+      ...rest,
+      owner: {
+        memberId,
+        nickname,
+        profileImage,
+      },
+    };
+  });
 };
 
-// TODO: 네이밍 맘에 안듬 + 로직을 분리해야할 듯?
-// slice & desc 임시
-const findAllByPostsIdOrderByIdDesc = (postsId, page) => {
-  const visibleComments = comments.filter((comment) =>
-    comment.postsId === postsId && comment.isVisible,
-  );
+const findByIdWithVisible = async ({ conn, id }) => {
+  const sql = `SELECT *
+               FROM comment
+               WHERE comment_id = ?
+                 AND is_visible = TRUE`;
+  const values = [id];
 
-  const totalComments = visibleComments.length;
+  let rows;
+  if (conn)
+    rows = await conn.query(sql, [id]);
+  else
+    rows = await query(sql, values);
 
-  const startIndex = Math.max(totalComments - (page * pageSize), 0);
-  const endIndex = Math.min(totalComments - ((page - 1) * pageSize), totalComments);
+  return conversionUtils.snakeToCamel(rows[0]);
+};
 
-  let result = {
-    hasNext: false,
-    nextPage: null,
-    data: null,
-  };
+const findByIdWithOwner = async ({ conn, id }) => {
+  const sql = `SELECT c.*, m.member_id, m.nickname, m.profile_image
+               FROM comment c
+                        JOIN member m ON c.owner_id = m.member_id
+               WHERE comment_id = ?
+                 AND is_visible = TRUE`;
+  const values = [id];
 
-  if (endIndex > 0) {
-    const sliceComments = visibleComments.slice(startIndex, endIndex);
-    const commentsSliceDesc = sliceComments.reverse();
-    if (startIndex !== 0) {
-      result.hasNext = true;
-      result.nextPage = page + 1;
-    }
-    result.data = commentsSliceDesc;
-    return result;
-  }
-  return result;  // data 가 null
+  let rows;
+  if (conn)
+    rows = await conn.query(sql, values);
+  else
+    rows = await query(sql, values);
+
+  return conversionUtils.snakeToCamel(rows[0]);
 };
 
 module.exports = {
   save,
   deleteById,
-  findById,
-  findAllByPostsIdOrderByIdDesc,
+  findByIdWithVisible,
+  findByIdWithOwner,
+  findAllByPostIdOrderByIdDesc,
 };

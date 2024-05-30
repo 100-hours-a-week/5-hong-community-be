99cf159cb9bbd454c47f132156aa54589ee06c0e
JiHongKim98 <kinjihong9598@gmail.com>
2024-05-31 01:05:47 +0900

    refact: Update post controller to use mysql database

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,120 +1,117 @@
-// TODO: 1) JWT 구현 후 권한 설정
-//       2) response dto
+// TODO: page -> cursor
 
 const { postDao } = require('../models');
-const { timeUtils, pagination } = require('../utils');
-
-// TODO: 게시글 작성자 임시 (JWT 구현중)
-// const tempPostsOwnerId = 1;
+const { command } = require('../config/database');
+const { timeUtils, cursorPageUtils } = require('../utils');
 
 const updateAllowField = ['title', 'contents', 'thumbnail']; // 수정 가능 부분
 
 // 5 게시글 씩 전송 (쿼리 요청 -> 5 +1 hasNext 필드)
 // 게시글 리스트(무한 스크롤) - [GET] "/api/v1/posts?page="
-const slicePostList = (req, res, next) => {
+const slicePostList = async (req, res, next) => {
   const { page } = req.query;
-  const nowPage = page ? parseInt(page, 10) : 1;
+  const cursor = page ? parseInt(page, 10) : null;
 
-  // hasNext, nextPage, data
-  const slicePosts = postDao.findAllOrderByIdDesc(nowPage);
-  // ownerId 의 세부정보로 변환
-  const paginationResponse = pagination.convertOwnerInfo(slicePosts);
+  const postList = await postDao.findAllOrderByIdDesc({ cursor });
 
-  return res.status(200).json(paginationResponse);
+  const maxViewNum = 5;
+  const baseCursor = 'postId';
+  const response = cursorPageUtils.sliceResponse(postList, maxViewNum, baseCursor);
+
+  return res.status(200).json(response);
 };
 
 // 게시글 상세 요청시 (hit + 1)
 // 게시글 상세 - [GET] "/api/v1/posts/{id}"
-const postsDetails = (req, res, next) => {
-  const postsId = parseInt(req.params.id);
+const postDetails = async (req, res, next) => {
+  const postId = parseInt(req.params.id);
 
-  const findPosts = postDao.findById(postsId);
-  if (!findPosts) {
+  const findPost = await postDao.findByIdWithOwner({ id: postId });
+  if (!findPost)
     return res.status(404).json({ message: '일치하는 게시글 없음' });
-  }
 
-  // 조회수 + 1
-  findPosts.hitsCount = findPosts.hitsCount + 1;
-  postDao.save(findPosts);
-
-  const response = pagination._convertOwnerInfo(findPosts);
-  return res.status(200).json(response);
+  return res.status(200).json(findPost);
 };
 
 // 게시글 생성 - [POST] "/api/v1/posts"
-const createPosts = (req, res, next) => {
+const createPost = async (req, res, next) => {
+  const currentMember = req.member; // session 기반
+
   const { title, contents, thumbnail } = req.body;
-  if (!title || !contents) {
+  if (!title || !contents)
     return res.status(400).json({ message: '필수 필드 누락' });
-  }
 
-  const nowMember = req.member; // session 기반
+  // transaction
+  return await command(async (conn) => {
+    const newPost = {
+      title,
+      contents,
+      createdAt: timeUtils.getCurrentTime(),
+      thumbnail,
+      commentsCount: 0,
+      hitsCount: 0,
+      likesCount: 0,
+      ownerId: currentMember.memberId,
+      isVisible: true,
+    };
+    await postDao.save({ conn, post: newPost });
 
-  const newPosts = {
-    title,
-    contents,
-    createdAt: timeUtils.getCurrentTime(),
-    thumbnail,
-    commentsCount: 0,
-    hitsCount: 0,
-    likesCount: 0,
-    // ownerId: tempPostsOwnerId,  // 임시 아이디
-    ownerId: nowMember.memberId,
-    isVisible: true,
-  };
-  postDao.save(newPosts);
-
-  return res.status(201).json({ message: '게시글 생성 완료' });
+    return res.status(201).json({ message: '게시글 생성 완료' });
+  });
 };
 
 // 게시글 수정 - [PUT] "/api/v1/posts/{id}"
-const updatePostsDetails = (req, res, next) => {
-  const postsId = parseInt(req.params.id);
+const updatePostDetails = async (req, res, next) => {
+  const currentMember = req.member; // session 기반
 
-  const findPosts = postDao.findById(postsId);
-  if (!findPosts) {
-    return res.status(404).json({ message: '일치하는 게시글 없음' });
-  }
+  const postId = parseInt(req.params.id);
 
-  const nowMember = req.member; // session 기반
-  if (findPosts.ownerId !== nowMember.memberId) {
-    return res.status(403).json({ message: '권한이 없으' });
-  }
+  // transaction
+  return await command(async (conn) => {
+    const findPost = await postDao.findByIdWithVisible({ conn, id: postId });
+    if (!findPost)
+      return res.status(404).json({ message: '일치하는 게시글 없음' });
 
-  Object.keys(req.body).forEach(key => {
-    if (updateAllowField.includes(key)) {
-      // 수정 허용 부분만 업뎃
-      findPosts[key] = req.body[key];
-    }
+    if (findPost.ownerId !== currentMember.memberId)
+      return res.status(403).json({ message: '권한이 없으' });
+
+    Object.keys(req.body).forEach(key => {
+      if (updateAllowField.includes(key)) {
+        // 수정 허용 부분만 업뎃
+        findPost[key] = req.body[key];
+      }
+    });
+    await postDao.save({ conn, post: findPost });
+
+    return res.status(204).end();
   });
-  postDao.save(findPosts);
-
-  return res.status(204).end();
 };
 
 // 게시글 삭제 - [DELETE] "/api/v1/posts/{id}"
-const deletePosts = (req, res, next) => {
-  const postsId = parseInt(req.params.id);
+const deletePost = async (req, res, next) => {
+  const currentMember = req.member; // session 기반
 
-  const findPosts = postDao.findById(postsId);
-  if (!findPosts) {
-    return res.status(404).json({ message: '존재하지 않는 게시글' });
-  }
+  const postId = parseInt(req.params.id);
 
-  const nowMember = req.member; // session 기반
-  if (findPosts.ownerId !== nowMember.memberId) {
-    return res.status(403).json({ message: '권한이 없으' });
-  }
+  // transaction
+  return await command(async (conn) => {
+    const findPost = await postDao.findByIdWithVisible({ conn, id: postId });
+    if (!findPost)
+      return res.status(404).json({ message: '존재하지 않는 게시글' });
 
-  postDao.deleteById(postsId);
+    if (findPost.ownerId !== currentMember.memberId)
+      return res.status(403).json({ message: '권한이 없으' });
 
-  return res.status(204).end();
+    await postDao.deleteById({ conn, id: postId });
+
+    return res.status(204).end();
+  });
 };
 
 module.exports = {
   slicePostList,
-  postsDetails,
-  createPosts,
-  updatePostsDetails,
-  deletePosts,
+  postDetails,
+  createPost,
+  updatePostDetails,
+  deletePost,
 };
